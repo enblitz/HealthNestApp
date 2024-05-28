4a72e87a0bea2e2272cd795122b9099ddb7b3aa5
NachiktaKadiya <kadiyanachiketa92@gmail.com>
2024-05-28 11:21:48 +0530

    dockerfiles updated

---
+++ backend/routes/doctorRoutes.js
@@ -0,0 +1,13 @@
+// doctorRoutes.js
+
+const express = require("express");
+const router = express.Router();
+const doctorController = require("../Controller/doctorController");
+
+router.get("/", doctorController.getAllDoctors);
+router.get("/:id", doctorController.getDoctorById);
+router.post("/", doctorController.createDoctor);
+router.put("/:id", doctorController.updateDoctorById);
+router.delete("/:id", doctorController.deleteDoctorById);
+
+module.exports = router;
