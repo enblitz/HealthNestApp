1ba4997cda10b70a17167ecd03427838dee3541f
Rushil1274 <rushilpanchal730@gmail.com>
2024-05-30 12:50:20 +0530

    Dockerfile updated

--- backend/db.js
+++ backend/db.js
@@ -19,5 +19,32 @@ db.connect((err) => {
     console.log("Connected to the MySQL database.");
   }
 });
+// const mysql = require("mysql2");
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+// console.log("DB_DATABASE:", process.env.DB_DATABASE);
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to the database:", err);
+//   } else {
+//     console.log("Connected to the MySQL database.");
+//     // Select the database
+//     db.query("USE " + process.env.DB_DATABASE, (err, result) => {
+//       if (err) {
+//         console.error("Error selecting database:", err);
+//       } else {
+//         console.log("Database selected successfully.");
+//       }
+//     });
+//   }
+// });
 
 module.exports = db;
