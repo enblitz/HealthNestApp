09a3e7381d230ece265d82213065a9bafb832470
IamJayBhandari <jaybhandari2404@gmail.com>
2024-06-10 18:05:20 +0530

    feat: doctor dashboard

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Navigate, Route } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { useUser } from './UserContext'; // Importing useUser hook assuming you have a UserContext
 
-function ProtectedRoute({ element, allowedRoles }) {
+function ProtectedRoute({ allowedRoles, userRole }) {
   const { user } = useUser();
   const isAuthenticated = user !== null;
+  console.log('ProtectedRoute:', { isAuthenticated, allowedRoles, userRole }); // Log props
 
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
   }
 
-  if (!allowedRoles.includes(user.role)) {
+  if (!allowedRoles.includes(userRole)) {
     return <Navigate to="/home" />;
   }
 
-  return <Route element={element} />;
+  return <Outlet />;
 }
 
 export default ProtectedRoute;
